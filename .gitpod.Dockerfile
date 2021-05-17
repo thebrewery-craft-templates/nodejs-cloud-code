3c83bcc174d867c7450d35b6d558d10231567fc2
Cellar Admin <rcdelacruz@gmail.com>
2021-05-17 10:56:30 +0000

    Update .gitpod.Dockerfile, .gitpod.yml, pgadmin4/setup-web-unattended.sh, pgadmin4/server.json files

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,2 +1,4 @@
 FROM gitpod/workspace-postgres
-RUN sudo apt-get update && sudo apt-get install -y libgtk-3-0 libx11-xcb1 libnss3 libxss1 libasound2
+RUN sudo apt-get update && sudo apt-get install -y libgtk-3-0 libx11-xcb1 libnss3 libxss1 libasound2 python3-virtualenv libpq-dev python3-dev netcat
+
+RUN pip install pgadmin4
---
+++ pgadmin4/setup-web-unattended.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -ex
+
+eval $(gp env --export)
+
+if [ -z "$PGADMIN_DEFAULT_EMAIL" ] ; then
+    gp env PGADMIN_DEFAULT_EMAIL=pgadmin@gitpod.io
+fi
+
+if [ -z "$PGADMIN_DEFAULT_PASSWORD" ] ; then
+    gp env PGADMIN_DEFAULT_PASSWORD=postgres
+fi
+
+eval $(gp env --export)
+
+export AUTOMATED=1
+export PGADMIN_SETUP_EMAIL=pgadmin@gitpod.io
+export PGADMIN_SETUP_PASSWORD=postgres
+export PGADMIN_CONFIG_DEFAULT_SERVER=0.0.0.0
+export PGADMIN_LISTEN_ADDRESS=0.0.0.0
+export PGADMIN_SERVER_JSON_FILE=./pgadmin4/servers.json
+
+/home/gitpod/.pyenv/shims/pgadmin4 &
+
+# wait for pgadmin to start
+while ! nc -z localhost 5050; do   
+  sleep 1
+done
+
+SETUP_SCRIPT_PATH=$(find /home/gitpod/.pyenv/versions -name setup.py | grep pgadmin4)
+
+/home/gitpod/.pyenv/shims/python $SETUP_SCRIPT_PATH  \
+    --load-servers $PGADMIN_SERVER_JSON_FILE \
+    --user pgadmin@gitpod.io
+
+echo "pgAdmin4 configured with credentials:
+$(gp env | grep PGADMIN_DEFAULT)"
+

e541aa5782165627cc0a67654f1760b8a2941c25
Ronald DC <apps@thebrewery.dev>
2021-07-18 17:28:20 +0800

    Added fs adapter

---
+++ cors.config.js
@@ -0,0 +1,19 @@
+/*
+ * Default filename: cors-config.js
+ * You can set here the CORS allowed origins.
+ * Removing/renaming this file will default to allow access from any origin.
+ */
+
+/*For example:
+module.exports = [
+  "http://localhost:8080",
+  "http://127.0.0.1:8080",
+  "https://showcase.domain.com",
+  "https://test.domain.com",
+];
+
+*/
+
+// This is the default setting, allows access from any origin.
+// You may add restrictions by adding domains (see example above).
+module.exports = [];
---
+++ parse.config.js
@@ -0,0 +1,30 @@
+/*
+ * PLEASE NOTE THAT THESE CONFIGURATIONS ARE FOR LOCAL DEVELOPMENT ONLY.
+ * ANY CHANGES HERE MAY NOT REFLECT ON YOUR DEPLOYED PROJECT IN CRAFT.
+ *
+ */
+
+module.exports = {
+  databaseURI:
+    process.env.DATABASE_URI ||
+    "postgres://postgres:postgres@localhost:5432/parse", //format: "postgres://user:password@localhost:5432/dbname"
+  appId: process.env.APP_ID || "myAppId",
+  clientKey: process.env.CLIENT_KEY || "myClientKey",
+  masterKey: process.env.MASTER_KEY || "myMasterKey",
+  serverURL: process.env.SERVER_URL || "http://localhost:1337/parse",
+  graphQLServerURL:
+    process.env.GRAPHQL_SERVER_URL || "http://localhost:1337/graphql",
+  javascriptKey: process.env.JAVASCRIPT_KEY || "myJSKey",
+  restAPIKey: process.env.REST_API_KEY || "myRestAPIKey",
+  cloud: process.env.PARSE_CLOUD_CODE || "./cloud/main.js",
+  liveQuery: {
+    classNames: [], // List of classes (from My Apps > Your App > Dasnboard > Browser) to support for query subscriptions (PubSub) example: [ 'User', 'Posts', 'Comments' ]
+  },
+  verbose: false,
+  filesAdapter: {
+    module: "@parse/fs-files-adapter",
+    options: {
+      encryptionKey: process.env.FILE_ENCRYPTION_KEY || "f1l33ncrypt10nk3y", //optional, but mandatory if you want to encrypt files
+    },
+  },
+};
